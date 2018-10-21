010f87730369df6574a69f9c8f2d95146800c261
Vignesh waran <vw170996@gmail.com>
2018-10-21 18:43:11 +0530

    Initial commit

---
+++ js/style.js
@@ -0,0 +1,10 @@
+// function widthFunc(width) {
+//     if (width.matches) { // If media query matches
+        
+//     }
+// }
+
+
+// var width = window.matchMedia("(max-width: 576px)")
+// myFunction(width) // Call listener function at run time
+// width.addListener(widthFunc);
