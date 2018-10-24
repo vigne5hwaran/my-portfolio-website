dc9167b0f34b8a8f6f1e5492be48fb334123d185
Vignesh waran <vw170996@gmail.com>
2018-10-24 22:45:29 +0530

    Design changes

--- assets/js/style.js
+++
@@ -1,9 +0,0 @@
-// function widthFunc(width) {
-//     if (width.matches) { // If media query matches
-        
-//     }
-// }
-
-// var width = window.matchMedia("(max-width: 576px)")
-// myFunction(width) // Call listener function at run time
-// width.addListener(widthFunc);
